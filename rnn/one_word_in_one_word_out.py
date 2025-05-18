52d9cbbaf4ce3081032eab85ee84c24e7ec872c2
Arturo <arturo.fierropardo@usp.ceu.es>
2025-05-18 13:05:57 +0200

    Feat: Función para el modelo line by line + prueba

--- rnn/one_word_in_one_word_out.py
+++ rnn/one_word_in_one_word_out.py
@@ -2,7 +2,7 @@
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, SimpleRNN, Dense
+from tensorflow.keras.layers import Embedding, LSTM, Dense
 import numpy as np
 
 def modelo1_owi_owo(text, epochs, n_words, seed_text):
@@ -18,7 +18,7 @@ def modelo1_owi_owo(text, epochs, n_words, seed_text):
     # Definir modelo
     model = Sequential()
     model.add(Embedding(input_dim=len(tokenizer.word_index)+1, output_dim=10))
-    model.add(SimpleRNN(50))
+    model.add(LSTM(50))
     model.add(Dense(len(tokenizer.word_index)+1, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.fit(x, y, epochs=epochs, verbose=0)
